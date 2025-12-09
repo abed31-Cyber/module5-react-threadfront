5081ee780457e70ba7d73ec79b3ac3af6af39b8e
Gaetan1303 <gaetan.begue13@gmail.com>
2025-12-09 09:27:31 +0100

    add gitignore

--- thread-front/src/App.jsx
+++ thread-front/src/App.jsx
@@ -1,24 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import HomePage from './components/HomePage.jsx'
-import Feed from './components/Feed.jsx'
-import PostCard from './components/PostCard.jsx'
+import HomePage from '@components/HomePage.jsx';
+import Feed from '@components/Feed/feed.jsx';
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Feed />} />
-          <Route path="/home" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/feed" element={<Feed />} />
           <Route path="*" element={<div>404 Not Found</div>} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
