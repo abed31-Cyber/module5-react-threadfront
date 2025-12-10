67b2c1f9b0633fbf048ff68ffa3af579b7ad3a24
Valer1712 <dreptatevaler1712@gmail.com>
2025-12-10 16:45:35 +0100

    resolution des colfits de branche

--- thread-front/src/App.jsx
+++ thread-front/src/App.jsx
@@ -19,7 +19,6 @@ function App() {
        <Link to="/">Accueil</Link>
        <Link to="/profile">profile</Link>
        <Link to="/feed">feed</Link>
-       <Link to="/comment">test unitaire comment</Link>
        
       </nav>
         <Routes>
@@ -29,7 +28,6 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/feed" element={<Feed />} />
           <Route path="/createpost" element={<CreatePost />} />
-          <Route path="/comment" element={<Commentary />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
