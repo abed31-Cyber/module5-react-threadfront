dbb97081ebb2bcbebb7ab823cd1189ab725276b1
abed31-Cyber <abedbek@gmail.com>
2025-12-10 14:19:01 +0100

    last commit

--- thread-front/src/App.jsx
+++ thread-front/src/App.jsx
@@ -1,11 +1,10 @@
-
 import './App.css'
 import { AuthProvider } from '../context/AuthContext.jsx'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Profile from './components/profile/Profile.jsx'
 import Commentary from './components/comment/comment.jsx'
 import Register from './components/Register.jsx'
-
+import logout from './utils/logout.jsx'
 import Login from './components/Login.jsx'
 import Feed from './components/Feed/feed.jsx'
 import CreatePost from './components/Post/CreatePost.jsx'
@@ -15,19 +14,18 @@ function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-      <nav>
+      {/* <nav>
        <Link to="/">Accueil</Link>
        <Link to="/profile">profile</Link>
-       <Link to="/feed">feed</Link>
-       
-      </nav>
+       <Link to="/">feed</Link>
+      </nav> */}
         <Routes>
- 
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/" element={<Feed />} />
+          <Route path="/profile" element={<><Profile /><button onClick={logout}>Déconnexion</button></>} />
           <Route path="/Login" element={<Login/>}/>
           <Route path="/register" element={<Register />} />
-          <Route path="/feed" element={<Feed />} />
           <Route path="/createpost" element={<CreatePost />} />
+           <Route path="/feed" element={<Feed />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
